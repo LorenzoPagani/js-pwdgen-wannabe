298e9ecfd9c08c9b297d887f2e935eb5eb15c8d3
Lorenzo Pagani <larrypg2@gmail.com>
2023-10-25 13:55:02 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+const firstName = prompt("Inserisci il tuo nome");
+const lastName = prompt("Inserisci il tuo cognome");
+const password = prompt("inserisci il tuo colore preferito");
+document.getElementById("userName").innerHTML = `Ciao, ${firstName}`;
+document.getElementById("password").innerHTML = `La tua password è: ${firstName}${lastName}${password}#23`;
